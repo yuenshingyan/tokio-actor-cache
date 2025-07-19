6af25a02e7ca71fc08ad94612c0ce37394aee89b
Yuen Shing Yan <yuenshingyan@gmail.com>
2025-07-19 21:19:22 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,10 @@ use crate::tokio_actor_cache::{hm::HashMapCache, hs::HashSetCache, vec::VecCache
 
 #[tokio::main]
 async fn main() {
-    // let hm_cache = HashMapCache::new(32).await;
+    let hm_cache = HashMapCache::new(32).await;
 
-    // // SET key in hm.
-    // if let Err(_) = hm_cache.insert("a", 10, Some(Duration::from_secs(1))).await {
-    //     println!("the receiver dropped")
-    // };
+    // SET key in hm.
+    hm_cache.insert("a", 10, None).await.expect("failed to insert key into hm");
 
     // // GET val with key from hm.
     // let val = hm_cache.get("a").await.expect("failed to get key");
@@ -28,6 +26,12 @@ async fn main() {
     // // GET val with key from hm.
     // let val = hm_cache.get("a").await.expect("failed to get key");
     // println!("{:?}", val);
+
+    let val = hm_cache.remove("a").await.expect("failed to remove key from hm");
+    println!("{:?}", val);
+
+    let val = hm_cache.get("a").await.expect("failed to get key");
+    println!("{:?}", val);
 
     // let hs_cache = HashSetCache::new(32).await;
 
@@ -51,24 +55,24 @@ async fn main() {
     // let hs = hs_cache.get().await;
     // println!("{:?}", hs);
 
-    let vec_cache = VecCache::<i32>::new(32).await;
+    // let vec_cache = VecCache::<i32>::new(32).await;
 
-    //
-    vec_cache
-        .push(99, Some(Duration::from_secs(3)))
-        .await
-        .expect("failed to insert into vec");
-    vec_cache
-        .push(99, Some(Duration::from_secs(1)))
-        .await
-        .expect("failed to insert into vec");
-    vec_cache
-        .push(99, None)
-        .await
-        .expect("failed to insert into vec");
+    // //
+    // vec_cache
+    //     .push(99, Some(Duration::from_secs(3)))
+    //     .await
+    //     .expect("failed to insert into vec");
+    // vec_cache
+    //     .push(99, Some(Duration::from_secs(1)))
+    //     .await
+    //     .expect("failed to insert into vec");
+    // vec_cache
+    //     .push(99, None)
+    //     .await
+    //     .expect("failed to insert into vec");
 
-    tokio::time::sleep(Duration::from_secs(2)).await;
+    // tokio::time::sleep(Duration::from_secs(2)).await;
 
-    let res = vec_cache.get().await.expect("failed to get vec");
-    println!("{:?}", res);
+    // let res = vec_cache.get().await.expect("failed to get vec");
+    // println!("{:?}", res);
 }
