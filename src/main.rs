e5cc081804724b0aec4444366de3852b58629407
Yuen Shing Yan <yuenshingyan@gmail.com>
2025-07-20 20:59:03 +0800

    add unittests for vec

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,10 @@ pub mod tokio_actor_cache {
 pub mod unittests {
     pub mod hm;
     pub mod hs;
+    pub mod vec;
 }
 
 #[tokio::main]
 async fn main() {
-    // let vec_cache = VecCache::<i32>::new(32).await;
-
-    // //
-    // vec_cache
-    //     .push(99, Some(Duration::from_secs(3)))
-    //     .await
-    //     .expect("failed to insert into vec");
-    // vec_cache
-    //     .push(99, Some(Duration::from_secs(1)))
-    //     .await
-    //     .expect("failed to insert into vec");
-    // vec_cache
-    //     .push(99, None)
-    //     .await
-    //     .expect("failed to insert into vec");
-
-    // tokio::time::sleep(Duration::from_secs(2)).await;
-
-    // let res = vec_cache.get().await.expect("failed to get vec");
-    // println!("{:?}", res);
+    
 }
---
+++ src/unittests/vec.rs
@@ -0,0 +1,60 @@
+#[cfg(test)]
+mod tests {
+
+    use std::time::Duration;
+
+    use crate::tokio_actor_cache::vec::VecCache;
+
+    #[tokio::test]
+    async fn test_clear() {
+        let vec_cache = VecCache::new(32).await;
+        vec_cache.push(10, None, None).await.unwrap();
+        vec_cache.push(20, None, None).await.unwrap();
+        vec_cache.push(30, None, None).await.unwrap();
+        let hs = vec_cache.get_all().await.unwrap();
+        assert_eq!(hs, Vec::from([10, 20, 30]));
+        vec_cache.clear().await.unwrap();
+        let hs = vec_cache.get_all().await.unwrap();
+        assert_eq!(hs.is_empty(), true);
+    }
+
+    #[tokio::test]
+    async fn test_remove() {
+        let vec_cache = VecCache::new(32).await;
+        vec_cache.push(10, None, None).await.unwrap();
+        let val = vec_cache.remove(10).await.unwrap();
+        assert_eq!(val, true);
+
+        let val = vec_cache.get_all().await.unwrap();
+        assert_eq!(val, Vec::new());
+    }
+
+    #[tokio::test]
+    async fn test_contains() {
+        let vec_cache = VecCache::new(32).await;
+        vec_cache.push(10, None, None).await.unwrap();
+        vec_cache.push(20, None, None).await.unwrap();
+        let val = vec_cache.contains(10).await.unwrap();
+        assert_eq!(val, true);
+    }
+
+    #[tokio::test]
+    async fn test_push_ex() {
+        let vec_cache = VecCache::new(32).await;
+        vec_cache.push(10, None, None).await.unwrap();
+        vec_cache.push(20, Some(Duration::from_secs(1)), None).await.unwrap();
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        let val = vec_cache.get_all().await.unwrap();
+        assert_eq!(val, Vec::from([10]));
+    }
+
+    #[tokio::test]
+    async fn test_push() {
+        let vec_cache = VecCache::new(32).await;
+        vec_cache.push(10, None, None).await.unwrap();
+        vec_cache.push(20, None, None).await.unwrap();
+        vec_cache.push(30, None, None).await.unwrap();
+        let val = vec_cache.get_all().await.unwrap();
+        assert_eq!(val, Vec::from([10, 20, 30]));
+    }
+}
