5d75909de42e6ad9fbfb0a075831305d8e354df4
Yuen Shing Yan <yuenshingyan@gmail.com>
2025-07-26 20:43:41 +0800

    add minsert and make other fn work with collection

--- src/tokio_actor_cache/hs.rs
+++ src/tokio_actor_cache/hs.rs
@@ -13,20 +13,25 @@ use tokio::time::{Instant, interval};
 #[derive(Debug)]
 pub enum HashSetCmd<V> {
     TTL {
-        val: V,
-        resp_tx: oneshot::Sender<Option<Duration>>,
+        vals: Vec<V>,
+        resp_tx: oneshot::Sender<Vec<Option<Duration>>>,
     },
     Clear,
     Remove {
-        val: V,
-        resp_tx: oneshot::Sender<bool>,
+        vals: Vec<V>,
+        resp_tx: oneshot::Sender<Vec<bool>>,
     },
     Contains {
-        val: V,
-        resp_tx: oneshot::Sender<bool>,
+        vals: Vec<V>,
+        resp_tx: oneshot::Sender<Vec<bool>>,
     },
     GetAll {
         resp_tx: oneshot::Sender<HashSet<V>>,
+    },
+    MInsert {
+        vals: Vec<V>,
+        ex: Option<Duration>,
+        nx: Option<bool>,
     },
     Insert {
         val: V,
@@ -40,10 +45,14 @@ pub struct HashSetCache<V> {
     pub tx: Sender<HashSetCmd<V>>,
 }
 
-impl<V> HashSetCache<V> {
-    pub async fn ttl(&self, val: V) -> Result<Option<Duration>, TokioActorCacheError> {
+impl<V> HashSetCache<V>
+where 
+    V: Clone,
+{
+    pub async fn ttl(&self, vals: &[V]) -> Result<Vec<Option<Duration>>, TokioActorCacheError> {
         let (resp_tx, resp_rx) = oneshot::channel();
-        let ttl_cmd = HashSetCmd::TTL { val, resp_tx };
+        let vals = vals.to_vec();
+        let ttl_cmd = HashSetCmd::TTL { vals, resp_tx };
         self.tx
             .try_send(ttl_cmd)
             .map_err(|_| TokioActorCacheError::Send)?;
@@ -59,9 +68,10 @@ impl<V> HashSetCache<V> {
             .map_err(|_| TokioActorCacheError::Send)
     }
 
-    pub async fn remove(&self, val: V) -> Result<bool, TokioActorCacheError> {
+    pub async fn remove(&self, vals: &[V]) -> Result<Vec<bool>, TokioActorCacheError> {
         let (resp_tx, resp_rx) = oneshot::channel();
-        let remove_cmd = HashSetCmd::Remove { val, resp_tx };
+        let vals = vals.to_vec();
+        let remove_cmd = HashSetCmd::Remove { vals, resp_tx };
         self.tx
             .try_send(remove_cmd)
             .map_err(|_| TokioActorCacheError::Send)?;
@@ -70,10 +80,11 @@ impl<V> HashSetCache<V> {
             .map_err(|_| return TokioActorCacheError::Receive)
     }
 
-    pub async fn contains(&self, val: V) -> Result<bool, TokioActorCacheError> {
+    pub async fn contains(&self, vals: &[V]) -> Result<Vec<bool>, TokioActorCacheError> {
         let (resp_tx, resp_rx) = oneshot::channel();
+        let vals = vals.to_vec();
         self.tx
-            .try_send(HashSetCmd::Contains { val, resp_tx })
+            .try_send(HashSetCmd::Contains { vals, resp_tx })
             .map_err(|_| TokioActorCacheError::Send)?;
         resp_rx
             .await
@@ -88,6 +99,18 @@ impl<V> HashSetCache<V> {
         resp_rx
             .await
             .map_err(|_| return TokioActorCacheError::Receive)
+    }
+
+    pub async fn minsert(
+        &self,
+        vals: &[V],
+        ex: Option<Duration>,
+        nx: Option<bool>,
+    ) -> Result<(), TokioActorCacheError> {
+        let vals = vals.to_vec();
+        self.tx
+            .try_send(HashSetCmd::MInsert { vals, ex, nx })
+            .map_err(|_| TokioActorCacheError::Send)
     }
 
     pub async fn insert(
@@ -123,15 +146,17 @@ impl<V> HashSetCache<V> {
                     command = rx.recv() => {
                         if let Some(cmd) = command {
                             match cmd {
-                                HashSetCmd::<V>::TTL { val, resp_tx } => {
-                                    let mut ttl = None;
-                                    for val_ex in &hs {
-                                        if val_ex.val == val {
-                                            ttl = val_ex.expiration.and_then(|ex| {
+                                HashSetCmd::<V>::TTL { vals, resp_tx } => {
+                                    let ttl = vals.into_iter().map(|val| {
+                                        let res = hs.iter().find(|val_ex| {
+                                            val_ex.val == val
+                                        });
+                                        res.and_then(|val_ex| {
+                                            val_ex.expiration.and_then(|ex| {
                                                 ex.checked_duration_since(Instant::now())
-                                            });
-                                        }
-                                    }
+                                            })
+                                        })
+                                    }).collect::<Vec<Option<Duration>>>();
                                     if let Err(_) = resp_tx.send(ttl) {
                                         println!("the receiver dropped");
                                     }
@@ -139,18 +164,25 @@ impl<V> HashSetCache<V> {
                                 HashSetCmd::<V>::Clear => {
                                     hs.clear();
                                 }
-                                HashSetCmd::<V>::Remove { val, resp_tx } => {
-                                    let is_exist = hs.iter().any(|val_ex| val_ex.val == val);
-                                    if is_exist {
-                                        hs.retain(|val_ex| val_ex.val != val);
-                                    }
+                                HashSetCmd::<V>::Remove { vals, resp_tx } => {
+                                    let is_exist = vals.into_iter().map(|val| {
+                                        let is_exist = hs.iter().any(|val_ex| val_ex.val == val);
+                                        if is_exist {
+                                            hs.retain(|val_ex| val_ex.val != val);
+                                            true
+                                        } else {
+                                            false
+                                        }
+                                    }).collect::<Vec<bool>>();
 
                                     if let Err(_) = resp_tx.send(is_exist) {
                                         println!("the receiver dropped");
                                     }
                                 }
-                                HashSetCmd::<V>::Contains { val, resp_tx } => {
-                                    let is_exist = hs.iter().any(|val_ex| val_ex.val == val);
+                                HashSetCmd::<V>::Contains { vals, resp_tx } => {
+                                    let is_exist = vals.into_iter().map(|val| {
+                                        hs.iter().any(|val_ex| val_ex.val == val)
+                                    }).collect::<Vec<bool>>();
                                     if let Err(_) = resp_tx.send(is_exist) {
                                         println!("the receiver dropped");
                                     }
@@ -163,6 +195,17 @@ impl<V> HashSetCache<V> {
                                         .clone();
                                     if let Err(_) = resp_tx.send(val) {
                                         println!("the receiver dropped");
+                                    }
+                                }
+                                HashSetCmd::<V>::MInsert { vals, ex, nx } => {
+                                    let expiration = ex.and_then(|d| Some(Instant::now() + d));
+                                    for val in vals {
+                                        let val_ex = ValueEx { val, expiration };
+                                        if nx.is_some() && nx == Some(true) && !hs.contains(&val_ex) {
+                                            hs.insert(val_ex);
+                                        } else {
+                                            hs.insert(val_ex);
+                                        }
                                     }
                                 }
                                 HashSetCmd::<V>::Insert { val, ex, nx } => {
