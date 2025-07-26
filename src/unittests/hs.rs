f6a51a663e1b3e90b46622e601ae0ab8aac2dfdf
Yuen Shing Yan <yuenshingyan@gmail.com>
2025-07-26 20:44:00 +0800

    update unittests

--- src/unittests/hs.rs
+++ src/unittests/hs.rs
@@ -11,8 +11,10 @@ mod tests {
             .insert(10, Some(Duration::from_secs(1)), None)
             .await
             .unwrap();
-        let ttl = hs_cache.ttl(10).await.unwrap();
-        assert!(Some(Duration::from_secs(1)) > ttl)
+        let ttl = hs_cache.ttl(&[10, 20]).await.unwrap();
+        println!("{:?}", ttl);
+        assert!(Some(Duration::from_secs(1)) > ttl[0]);
+        assert_eq!(ttl[1], None);
     }
 
     #[tokio::test]
@@ -31,20 +33,34 @@ mod tests {
     #[tokio::test]
     async fn test_remove() {
         let hs_cache = HashSetCache::new(32).await;
-        hs_cache.insert(10, None, None).await.unwrap();
-        let val = hs_cache.remove(10).await.unwrap();
-        assert_eq!(val, true);
-
-        let val = hs_cache.get_all().await.unwrap();
-        assert_eq!(val, HashSet::new());
+        hs_cache.minsert(&[10, 20, 30], None, None).await.unwrap();
+        let vals = hs_cache.remove(&[10, 20, 30, 40]).await.unwrap();
+        assert_eq!(vals, vec![true, true, true, false]);
     }
 
     #[tokio::test]
     async fn test_contains() {
         let hs_cache = HashSetCache::new(32).await;
         hs_cache.insert(10, None, None).await.unwrap();
-        let val = hs_cache.contains(10).await.unwrap();
-        assert_eq!(val, true);
+        let vals = hs_cache.contains(&[10]).await.unwrap();
+        assert_eq!(vals, vec![true]);
+    }
+
+    #[tokio::test]
+    async fn test_minsert_ex() {
+        let hs_cache = HashSetCache::new(32).await;
+        hs_cache.minsert(&[10, 20, 30], Some(Duration::from_secs(1)), None).await.unwrap();
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        let vals = hs_cache.get_all().await.unwrap();
+        assert_eq!(vals, HashSet::<i32>::new());
+    }
+
+    #[tokio::test]
+    async fn test_minsert() {
+        let hs_cache = HashSetCache::new(32).await;
+        hs_cache.minsert(&[10, 20, 30], None, None).await.unwrap();
+        let val = hs_cache.get_all().await.unwrap();
+        assert_eq!(val, HashSet::from([10, 20, 30]));
     }
 
     #[tokio::test]
