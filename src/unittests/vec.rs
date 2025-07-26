a1c928d9f4d6dd5c9bb49f88abba2ea09b7872f0
Yuen Shing Yan <yuenshingyan@gmail.com>
2025-07-26 20:55:24 +0800

    update unittests

--- src/unittests/vec.rs
+++ src/unittests/vec.rs
@@ -12,8 +12,9 @@ mod tests {
             .push(10, Some(Duration::from_secs(1)), None)
             .await
             .unwrap();
-        let ttl = vec_cache.ttl(10).await.unwrap();
-        assert!(Some(Duration::from_secs(1)) > ttl)
+        let ttl = vec_cache.ttl(&[10, 20]).await.unwrap();
+        assert!(Some(Duration::from_secs(1)) > ttl[0]);
+        assert_eq!(ttl[1], None);
     }
 
     #[tokio::test]
@@ -33,11 +34,8 @@ mod tests {
     async fn test_remove() {
         let vec_cache = VecCache::new(32).await;
         vec_cache.push(10, None, None).await.unwrap();
-        let val = vec_cache.remove(10).await.unwrap();
-        assert_eq!(val, true);
-
-        let val = vec_cache.get_all().await.unwrap();
-        assert_eq!(val, Vec::new());
+        let val = vec_cache.remove(&[10, 20]).await.unwrap();
+        assert_eq!(val, vec![true, false]);
     }
 
     #[tokio::test]
@@ -45,8 +43,25 @@ mod tests {
         let vec_cache = VecCache::new(32).await;
         vec_cache.push(10, None, None).await.unwrap();
         vec_cache.push(20, None, None).await.unwrap();
-        let val = vec_cache.contains(10).await.unwrap();
-        assert_eq!(val, true);
+        let val = vec_cache.contains(&[10, 20, 30]).await.unwrap();
+        assert_eq!(val, vec![true, true, false]);
+    }
+
+    #[tokio::test]
+    async fn test_mpush_ex() {
+        let vec_cache = VecCache::new(32).await;
+        vec_cache.mpush(&[10, 20, 30], Some(Duration::from_secs(1)), None).await.unwrap();
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        let val = vec_cache.get_all().await.unwrap();
+        assert_eq!(val, Vec::new());
+    }
+
+    #[tokio::test]
+    async fn test_mpush() {
+        let vec_cache = VecCache::new(32).await;
+        vec_cache.mpush(&[10, 20, 30], None, None).await.unwrap();
+        let val = vec_cache.get_all().await.unwrap();
+        assert_eq!(val, Vec::from([10, 20, 30]));
     }
 
     #[tokio::test]
