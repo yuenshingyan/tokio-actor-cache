49163038bfb06d74607b6c1267a1aab09cf4a90f
Yuen Shing Yan <yuenshingyan@gmail.com>
2025-07-26 19:29:53 +0800

    add enum variant InconsistentLen

--- src/tokio_actor_cache/error.rs
+++ src/tokio_actor_cache/error.rs
@@ -3,6 +3,8 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum TokioActorCacheError {
     #[error("unknown data store error")]
+    InconsistentLen,
+    #[error("unknown data store error")]
     Receive,
     #[error("unknown data store error")]
     Send,
