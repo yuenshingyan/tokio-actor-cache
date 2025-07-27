190ed10b94d177a743b2a108f592c8bac8ff5e77
Yuen Shing Yan <yuenshingyan@gmail.com>
2025-07-27 16:03:37 +0800

    add enum var NodeNotExists

--- src/tokio_actor_cache/error.rs
+++ src/tokio_actor_cache/error.rs
@@ -2,6 +2,8 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum TokioActorCacheError {
+    #[error("node cannot be found")]
+    NodeNotExists,
     #[error("unknown data store error")]
     InconsistentLen,
     #[error("unknown data store error")]
