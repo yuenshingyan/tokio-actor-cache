bde2e1bb2480bf25fe399ce4a4c83334dcc69201
Yuen Shing Yan <yuenshingyan@gmail.com>
2025-08-03 14:58:03 +0800

    add hs state and add call_cnt and last_accessed

--- src/tokio_cache/data_struct.rs
+++ src/tokio_cache/data_struct.rs
@@ -1,7 +1,16 @@
 use tokio::time::Instant;
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct HashSetState {
+    pub expiration: Option<Instant>,
+    pub call_cnt: u64,
+    pub last_accessed: Instant,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct ValueEx<V> {
     pub val: V,
     pub expiration: Option<Instant>,
+    pub call_cnt: u64,
+    pub last_accessed: Instant,
 }
